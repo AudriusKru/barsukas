f9c2693e0b4dd7cf12cba293973e09de01e8634c
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-03 10:40:48 +0300

    masyvai

---
+++ Masyvai-1/01.php
@@ -0,0 +1,63 @@
+<?php
+
+echo '1. Uždavinio atsakymas:';
+echo '<br>';
+
+// $elemetai = 30;
+
+// for ($i = 0; $i < $elemetai; $i++) { 
+//     $masyvas[] = rand(5, 25);
+// }
+
+$elemetai = 30;
+$masyvas = array_fill(0, $elemetai, ' ');
+foreach ($masyvas as $index => &$random) {
+    $random = rand(5, 25);
+}
+
+echo '<br>';echo '<br><hr>';echo '<br>';
+
+echo '2.A Uždavinio atsakymas:';
+echo '<br>';
+
+$kiek = 0;
+foreach ($masyvas as $index => $random) {
+    if ($random > 10) {
+        $kiek++;
+    }
+}
+echo 'reiksmiu didesniu nei 10 yra:' .$kiek .'<br>' .'<br><hr>';
+
+echo '2.B Uždavinio atsakymas:';
+echo '<br>';
+
+
+echo '2.C Uždavinio atsakymas:';
+echo '<br>';
+
+echo '2.D Uždavinio atsakymas:';
+echo '<br>';
+
+echo '2.E Uždavinio atsakymas:';
+echo '<br>';
+
+echo '2.F Uždavinio atsakymas:';
+echo '<br>';
+
+echo '2.G Uždavinio atsakymas:';
+echo '<br>';
+
+echo '2.H Uždavinio atsakymas:';
+echo '<br>';
+
+echo '2.I Uždavinio atsakymas:';
+echo '<br>';
+
+echo '3. Uždavinio atsakymas:';
+echo '<br>';
+
+echo '4. Uždavinio atsakymas:';
+echo '<br>';
+
+
+

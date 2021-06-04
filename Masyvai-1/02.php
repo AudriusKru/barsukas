5f79c15837f32637bd614ba4b8fa4444e3a60c09
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-04 18:42:14 +0300

    2d

--- Masyvai-1/02.php
+++ Masyvai-1/02.php
@@ -1,9 +1,7 @@
 <?php
 
-//// Naudodamiesi 2 uždavinio masyvu:
-// //Suskaičiuokite kiek masyve yra reikšmių didesnių už 10;
-// //Raskite didžiausią masyvo reikšmę ir jos indeksą arba indeksus jeigu yra keli;
-// Suskaičiuokite visų porinių (lyginių) indeksų reikšmių sumą;
+
+
 // Sukurkite naują masyvą, kurio reikšmės yra 1 uždavinio masyvo reikšmes minus tos reikšmės indeksas;
 // Papildykite masyvą papildomais 10 elementų su reikšmėmis nuo 5 iki 25, kad bendras masyvas padidėtų iki indekso 39;
 // Iš masyvo elementų sukurkite du naujus masyvus. Vienas turi būti sudarytas iš neporinių indekso reikšmių, o kitas iš porinių;
@@ -11,10 +9,10 @@
 // Suraskite pirmą (mažiausią) indeksą, kurio elemento reikšmė didesnė už 10;
 // Naudodami funkciją unset() iš masyvo ištrinkite visus elementus turinčius porinį indeksą;
 
-
-
 echo '2.A Uždavinio atsakymas:';
 echo '<br>';
+//// Naudodamiesi 2 uždavinio masyvu:
+// //Suskaičiuokite kiek masyve yra reikšmių didesnių už 10;
 
 $elemetai = 30;
 $masyvas = array_fill(0, $elemetai, ' ');
@@ -34,9 +32,16 @@ echo '<br><hr>';
 
 echo '2.B Uždavinio atsakymas:';
 echo '<br>';
-$masyvo = max($masyvas);
-$indekso =array_search(max($masyvas),$masyvas);
-echo "Max reiksmes: masyvo - $masyvo, indekso - $indekso .";
+//Raskite didžiausią masyvo reikšmę ir jos indeksą arba indeksus jeigu yra keli;
+$max_reiksme = $masyvas[0];
+$max_indeksas = 0;
+foreach ($masyvas as $indexas => $sk) {
+    if ($sk >  $max_reiksme) {
+        $max_reiksme = $sk;
+        $max_indeksas = $indexas;
+    }
+}
+echo "Max reiksmes: masyvo - $max_reiksme, indekso - $max_indeksas.";
 echo '<br><hr>';
 
 echo '2.C Uždavinio atsakymas:';
@@ -50,11 +55,24 @@ foreach ($masyvas as $index  => $sk) {
     }
 }
 
-echo "$lyginiai_indexsai";
+echo "Visų porinių (lyginių) indeksų reikšmių suma: $lyginiai_indexsai.";
 echo '<br><hr>';
 
 echo '2.D Uždavinio atsakymas:';
 echo '<br>';
+
+// Sukurkite naują masyvą, kurio reikšmės yra 1 uždavinio masyvo reikšmes minus tos reikšmės indeksas;
+
+$naujas_masyvas = [];
+
+foreach ($masyvas as $index => $sk) {
+    $naujas_masyvas [] = ($sk - $index);
+}
+
+echo 'Naujo masyvo reikšmes minus tos reikšmės indeksas:';
+echo '<br>';
+print_r($naujas_masyvas);
+echo '<br><hr>';
 
 echo '2.E Uždavinio atsakymas:';
 echo '<br>';

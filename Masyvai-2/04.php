72e1381e8d4fc8bc5a2e51576dc977464ec2e705
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-07 21:07:12 +0300

    no.4

--- Masyvai-2/04.php
+++ Masyvai-2/04.php
@@ -2,3 +2,11 @@
 
 echo '4. Uždavinio atsakymas:';
 echo '<br>';
+
+$masyvas = array_fill(0, 30, 0);
+
+foreach ($masyvas as $xxx => $value_xxx) {
+    $masyvas[$xxx] = ['user_i' => rand(1, 1000000), 'place_in_row' => rand(0, 100)];
+}
+
+_d($masyvas);

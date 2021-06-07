2ff913927a4859752cdb028d7edf86f667bf97bd
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-07 12:29:36 +0300

    monday

--- Masyvai-1/06.php
+++ Masyvai-1/06.php
@@ -5,3 +5,26 @@ echo '<br>';
 
 
 // Sugeneruokite du masyvus, kurių reikšmės yra atsitiktiniai skaičiai nuo 100 iki 999. Masyvų ilgiai 100. Masyvų reikšmės turi būti unikalios savo masyve (t.y. neturi kartotis).
+
+$masyvas1 = [];
+$masyvas2 = [];
+
+foreach (range(1, 100) as $value) {
+    $random = rand(100, 999);
+    $masyvas1[] = $random;
+    // $value = $atsitiktinis_1;
+}
+
+// print_r($masyvas1);
+
+echo 'Pirmas masyvas:<br>';
+print_r(array_unique($masyvas1));
+echo '<br><hr>';
+
+foreach (range(1, 100) as $value) {
+    $random = rand(100, 999);
+    $masyvas2[] = $random;
+}
+
+echo 'Antras masyvas:<br>';
+print_r(array_unique($masyvas2));
--- Masyvai-1/pp.php
+++ Masyvai-1/pp.php
@@ -1,36 +1,6 @@
 <?php
 
-echo "5-as uzdavinys";
-echo "<br>";
-echo "<br>";
 
-$raides = ['A', 'B', 'C', 'D'];
-$raidziuMasyvas1 = array_fill(0, 200, 0);
-
-foreach ($raidziuMasyvas1 as $key => &$value) {
-    $value = $raides[rand(0, count($raides)-1)];
-}
-unset($value);
-
-$raidziuMasyvas2 = array_fill(0, 200, 0);
-
-foreach ($raidziuMasyvas2 as $key => &$value) {
-    $value = $raides[rand(0, count($raides)-1)];
-}
-unset($value);
-
-$raidziuMasyvas3 = array_fill(0, 200, 0);
-
-foreach ($raidziuMasyvas3 as $key => &$value) {
-    $value = $raides[rand(0, count($raides)-1)];
-}
-unset($value);
-
-$didelisMasyvas = [];
-
-foreach ($raidziuMasyvas1 as $key => $value) {
-    $didelisMasyvas[] = $value . $raidziuMasyvas2[$key] . $raidziuMasyvas3[$key];
-}
-
-print_r($didelisMasyvas);
-
+$input = array("a" => "green", "red", "b" => "green", "blue", "red");
+$result = array_unique($input);
+print_r($result);

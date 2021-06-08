9c312e646e53600d167b49a2882eeebb2c2a826c
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-08 17:10:57 +0300

    no.4

--- Funkcijos/04.php
+++ Funkcijos/04.php
@@ -1,2 +1,21 @@
 <?php
 
+echo '4. Uždavinio atsakymas:';
+echo '<br>';
+
+function numberis($number) {
+    $count = 0;
+    for ($i = 2; $i < $number ; $i++) { 
+        if ($number % $i === 0) {
+            $count++;
+        } 
+        if ($count === 0){
+            return 'Dalyba negalima.';
+        }
+    }       
+     return "Skaičius: $number dalijasi iš $count sveikų skaičių.";
+}
+
+$skaicius = rand(2, 1500);
+
+echo numberis($skaicius);

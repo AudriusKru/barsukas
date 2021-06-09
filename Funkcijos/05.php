cae84c43db5816e344eb5ae870d0656a94c8d6bf
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-09 11:02:41 +0300

    no.5

--- Funkcijos/05.php
+++ Funkcijos/05.php
@@ -1,8 +1,6 @@
 <?php
 
-echo '5. Uždavinio atsakymas:';
-echo '<br>';
-
+echo '5. Uždavinio atsakymas:<br>';
 
 for ($i = 0; $i < 100; $i++) { 
     $masyvas[] = rand(333, 777);
@@ -12,3 +10,39 @@ usort($masyvas, function($a, $b) {
 });
 
 print_r($masyvas);
+
+
+//----------------------------------destytojo
+
+function divisor(int $digit) : int
+{
+    if ($digit === 0 || $digit === 1) {
+        return 1;
+    }
+    if ($digit === 2) {
+        return 0;
+    }
+    $divisor = range(2, $digit - 1);
+    // $div = 0;
+    $div_counter = 0;
+    foreach($divisor as $div) {
+        if ($digit % $div === 0) {
+            $div_counter++;
+        }
+    }
+    return $div_counter;
+}
+// cia ketirtas
+//---------------------------5----------------
+
+$mas100 = [];
+
+foreach(range(1, 100) as $_) {
+    $mas100[] = rand(33, 77);
+}
+
+usort($mas100, function($a, $b) {
+    return divisor($b) <=> divisor($a);
+});
+
+_d($mas100);

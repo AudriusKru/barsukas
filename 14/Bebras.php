e50172353873b5a7943a3b02dd0809458d5dbef6
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-17 12:18:22 +0300

    public

---
+++ 14/Bebras.php
@@ -0,0 +1,20 @@
+<?php
+
+// Programuotoja Zita
+
+class Bebras {
+// savybes
+    public $name = 'Sarikas';
+    protected $color = 'brown';
+    private $age = 24;
+    //----------------------------------------
+
+// metodai
+
+    public function sayHello() 
+    {
+        echo '<h2>Hello!</h2>';
+    }
+
+
+}
--- objektinis/Bebras.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Programuotojas Zita
-
-class Bebras {
-
-}

f39ca6976a5d5531c3213be291556efa1993ba98
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-17 11:30:24 +0300

    getas postas

---
+++ 14/get-Post.php
@@ -0,0 +1,12 @@
+<?php
+_d($_SERVER['REQUEST_METHOD'], 'METHOD:');
+_d($_GET, 'GET');
+_d($_POST, 'POST');
+
+?>
+<a href="http://localhost/barsukas/14/get-Post.php">back</a>
+<br></br>
+<form action="http://localhost/barsukas/14/get-Post.php?island=1" method="get">
+<input type="hidden" name="alio" value="reklama">
+<button type="submit" name="jaja" value="ding-dong">button</button>
+</form>

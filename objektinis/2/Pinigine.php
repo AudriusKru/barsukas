b01857a1e8c487e7cb4baa628b0f7229d44e0fb7
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-22 19:33:02 +0300

    start money

---
+++ objektinis/2/Pinigine.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class Pinigine 
+{
+    private $popieriniaiPinigai;
+    private $metaliniaiPinigai;
+
+    private function ideti($kiekis) 
+    {
+        if ($kiekis < 2) {
+            return "ERROR";
+        }
+
+        if ($kiekis <= 2) {
+            return $this->metaliniaiPinigai += $kiekis;
+        }
+        elseif {
+            return $this->popieriniaiPinigai +=$kiekis;
+        }
+    }
+}
---
+++ objektinis/2/index.php
@@ -0,0 +1,6 @@
+<?php
+
+
+require __DIR__ . '/Pinigine.php';
+
+$Pinigine = new Pinigine;

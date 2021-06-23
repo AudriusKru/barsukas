dff042c708742c5701fca4f502ad4b3c7294efa5
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-23 12:20:08 +0300

    save

--- 16/Tv.php
+++ 16/Tv.php
@@ -4,7 +4,7 @@ class Tv
 {
 
     protected $istrizaine;
-    public static $kanalai = [1 => 'TV3', 2 => 'LRT', 3 => 'Polonia'];
+    private static $kanalai = [1 => 'TV3', 2 => 'LRT', 3 => 'Polonia'];
 
 
     public function __construct($ist)
@@ -16,4 +16,10 @@ class Tv
     {
         echo '<h1>ROOOODOOM: '.self::$kanalai[$kanalas].'</h1>';
     }
+
+    public static function keistiPrograma($kanalas, $pavadinimas)
+    {
+        self::$kanalai[$kanalas] = $pavadinimas;
+    }
+
 }
--- 16/index.php
+++ 16/index.php
@@ -10,6 +10,7 @@ $TV3 = new Tv(46);
 // $TV1->kanalai[1] = 'LRT2';
 
 $TV1::$kanalai[1] = 'LRT2';
+TV1::keistiPrograma(1, 'LRT101');
 
 $TV1->rodyti(1);
 $TV2->rodyti(1);

edd413ed299ec40d07f3a899d0b80fc58bbee908
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-23 15:03:21 +0300

    finisk wallet

--- objektinis/2/index.php
+++ objektinis/2/index.php
@@ -7,15 +7,22 @@ $Pinigine = new Pinigine;
 
 echo "Antra užduotis:<br><hr>";
 
-echo 'Metaliniai pinigai: ' .$Pinigine->ideti(rand(-2,2));
-echo '<br><hr>'; 
-echo 'Popieriniai pinigai: ' .$Pinigine->ideti(rand(-10,10));
-echo '<br><hr>'; 
+// echo 'Metaliniai pinigai: ' .$Pinigine->ideti(rand(-2,2));
+// echo '<br><hr>'; 
+// echo 'Popieriniai pinigai: ' .$Pinigine->ideti(rand(-10,10));
+// echo '<br><hr>'; 
 
-echo "Suma: ";
-echo $Pinigine->skaiciuoti();
-echo '<br><hr>'; 
+// echo "Suma: ";
+// echo $Pinigine->skaiciuoti();
+// echo '<br><hr>'; 
 
 
 
 //////////////////////////// Destytojo
+
+$Pinigine->skaiciuoti();
+
+$Pinigine->ideti(5);
+$Pinigine->ideti(0.33);
+
+$Pinigine->skaiciuoti();

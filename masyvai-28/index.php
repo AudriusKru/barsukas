8e15daef89b15b936309bba65624c4d09947539e
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-29 10:03:27 +0300

    paskaita

---
+++ masyvai-28/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$masyas = [];
+
+$masyas1 = [1, 3, 14];
+
+$masyas2 = array();
+
+$masyas3 = array(1, 3, 14);
+
+$masyas4 = ['h' => 1, 8 => 3, 'd' => 14];
+
+_dc($masyas4);
+
+krsort($masyas4);
+_dc($masyas4);

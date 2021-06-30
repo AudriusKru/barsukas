4d248c657666357b9224a8660f3e6dc0280c45dd
AudriusKru <krutulis.audrius@gmail.cm>
2021-06-30 11:55:53 +0300

    gryas ount

--- objektinis/7/Grybas.php
+++ objektinis/7/Grybas.php
@@ -9,6 +9,7 @@ class Grybas {
         $this->valgomas = rand(0, 1) ? true : false;
         $this->sukirmijes = rand(0, 1) ? true : false;
         $this->svoris = rand(5, 45);
+        // echo '<h6>Grybas</h6>';
     }
 
     public function __get($prop)

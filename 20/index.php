5fc75eddc139dfc5b2e50c37ee1b0417db7007c2
AudriusKru <krutulis.audrius@gmail.cm>
2021-07-01 16:03:55 +0300

    composer init

--- 20/index.php
+++ 20/index.php
@@ -12,58 +12,63 @@ use Belgium\Animal as B;
 //base_dir yra egzistuojantis folderis kurio nera namespace 
 // principas kaip kodas iesko klase jei nori klases Zoo\Animal zoo folderis clase animal
 
-spl_autoload_register(function ($class) {
-    $prefix = 'Zoo\\';
-    $base_dir = __DIR__ . '/';
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
+// spl_autoload_register(function ($class) {
+//     $prefix = 'Zoo\\';
+//     $base_dir = __DIR__ . '/';
+//     $len = strlen($prefix);
+//     if (strncmp($prefix, $class, $len) !== 0) {
+//         return;
+//     }
 
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
+//     $relative_class = substr($class, $len);
+//     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+//     if (file_exists($file)) {
+//         require $file;
+//     }
+// });
 
-spl_autoload_register(function ($class) {
-    $prefix = 'Australia\\';
-    $base_dir = __DIR__ . '/australai/';
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
+// spl_autoload_register(function ($class) {
+//     $prefix = 'Australia\\';
+//     $base_dir = __DIR__ . '/australai/';
+//     $len = strlen($prefix);
+//     if (strncmp($prefix, $class, $len) !== 0) {
+//         return;
+//     }
 
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
-spl_autoload_register(function ($class) {
-    $prefix = 'Belgium\\';
-    $base_dir = __DIR__ . '/belgai/';
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
+//     $relative_class = substr($class, $len);
+//     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+//     if (file_exists($file)) {
+//         require $file;
+//     }
+// });
+// spl_autoload_register(function ($class) {
+//     $prefix = 'Belgium\\';
+//     $base_dir = __DIR__ . '/belgai/';
+//     $len = strlen($prefix);
+//     if (strncmp($prefix, $class, $len) !== 0) {
+//         return;
+//     }
 
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
+//     $relative_class = substr($class, $len);
+//     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+//     if (file_exists($file)) {
+//         require $file;
+//     }
+// });
+
+
 // require __DIR__ . '/australai/Animal.php';
 // require __DIR__ . '/belgai/Animal.php';
 // require __DIR__ . '/Animal.php';
+
 
 echo Animal::$hello;
 // new Animal;
 echo '<br>';
 echo AnimalA::$go;
 // new AnimalA;
+echo '<br>';
+echo Animal::$hello;
 echo '<br>';
 echo B::$go;
 // new B;

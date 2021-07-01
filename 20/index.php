623c5b661140de3300ff2c30050bf40f19227821
AudriusKru <krutulis.audrius@gmail.cm>
2021-07-01 15:50:15 +0300

    spal autoload register

--- 20/index.php
+++ 20/index.php
@@ -5,36 +5,59 @@ namespace Zoo;
 use Zoo\Animal;
 use Australia\Animal as AnimalA;
 use Belgium\Animal as B;
+// require __DIR__ .'/vendor/autoload.php';
 
-require __DIR__ .'/vendor/autoload.php';
+// php siulo funkcija sp_autoload();
+//prefix yra ta namespace dalis kuri neatitinka folderio strukturos  mes turime (zoo animal bet musu folderio zoo nera tai mes sakome kad sori bet pas mus toks dalykas kaip zoo folderi neegzituoja)
+//base_dir yra egzistuojantis folderis kurio nera namespace 
+// principas kaip kodas iesko klase jei nori klases Zoo\Animal zoo folderis clase animal
 
-//php siulo funkcija sp_autoload();
+spl_autoload_register(function ($class) {
+    $prefix = 'Zoo\\';
+    $base_dir = __DIR__ . '/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
 
-// spl_autoload_register(function ($class) {
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
 
-//     echo "<h3>1. Nerandu klasės: $class.</h3>";
-//     echo '<br>';
-// });
+spl_autoload_register(function ($class) {
+    $prefix = 'Australia\\';
+    $base_dir = __DIR__ . '/australai/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
 
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+spl_autoload_register(function ($class) {
+    $prefix = 'Belgium\\';
+    $base_dir = __DIR__ . '/belgai/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
 
-// spl_autoload_register(function ($class) {
-
-//     echo "<h3>2. Nerandu klasės: $class.</h3>";
-//     echo '<br>';
-//     // require __DIR__ . '/australai/Animal.php';
-// });
-
-// spl_autoload_register(function ($class) {
-
-//     echo "<h3>3. Nerandu klasės: $class.</h3>";
-//     echo '<br>';
-// });
-
-
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
 // require __DIR__ . '/australai/Animal.php';
 // require __DIR__ . '/belgai/Animal.php';
 // require __DIR__ . '/Animal.php';
-
 
 echo Animal::$hello;
 // new Animal;

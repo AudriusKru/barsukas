3a35ce2b38b558441e098ee106049c77e1a4e892
AudriusKru <krutulis.audrius@gmail.cm>
2021-07-01 10:56:31 +0300

    name space

---
+++ 20/australai/Animal.php
@@ -0,0 +1,6 @@
+<?php
+namespace Australia;
+
+class Animal {
+    
+}
---
+++ 20/index.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__.'/Animal.php';
+require __DIR__.'/belgai/Animal.php';
+require __DIR__.'/australai/Animal.php';

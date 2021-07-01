f053f7f7d8adab957cc98c4b77c42116678b770e
AudriusKru <krutulis.audrius@gmail.cm>
2021-07-01 11:51:05 +0300

    hide

--- 20/index.php
+++ 20/index.php
@@ -8,30 +8,30 @@ use Belgium\Animal as B;
 
 //php siulo funkcija sp_autoload();
 
-spl_autoload_register(function ($class) {
+// spl_autoload_register(function ($class) {
 
-    echo "<h3>1. Nerandu klasės: $class.</h3>";
-    echo '<br>';
-});
-
-
-spl_autoload_register(function ($class) {
-
-    echo "2. Nerandu klasės: $class.";
-    echo '<br>';
-    require __DIR__ . '/australai/Animal.php';
-});
-
-spl_autoload_register(function ($class) {
-
-    echo "<h3>3. Nerandu klasės: $class.</h3>";
-    echo '<br>';
-});
+//     echo "<h3>1. Nerandu klasės: $class.</h3>";
+//     echo '<br>';
+// });
 
 
-// 
-require __DIR__ . '/belgai/Animal.php';
-require __DIR__ . '/Animal.php';
+// spl_autoload_register(function ($class) {
+
+//     echo "<h3>2. Nerandu klasės: $class.</h3>";
+//     echo '<br>';
+//     // require __DIR__ . '/australai/Animal.php';
+// });
+
+// spl_autoload_register(function ($class) {
+
+//     echo "<h3>3. Nerandu klasės: $class.</h3>";
+//     echo '<br>';
+// });
+
+
+// require __DIR__ . '/australai/Animal.php';
+// require __DIR__ . '/belgai/Animal.php';
+// require __DIR__ . '/Animal.php';
 
 
 echo Animal::$hello;
